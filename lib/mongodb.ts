79043ffb3759db7da9c2d9e971c3a8cdc6206b7b
SantinoSchiavoni <2319332@ucc.edu.ar>
2025-12-20 18:45:37 -0300

    avances 3

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -2,17 +2,7 @@ import mongoose from 'mongoose';
 
 const MONGODB_URI = process.env.MONGODB_URI;
 
-if (!MONGODB_URI) {
-  throw new Error(
-    'Please define the MONGODB_URI environment variable inside .env.local'
-  );
-}
-
-/**
- * Global is used here to maintain a cached connection across hot reloads
- * in development. This prevents connections growing exponentially
- * during API Route usage.
- */
+// Cached handler
 interface MongooseCache {
   conn: mongoose.Connection | null;
   promise: Promise<mongoose.Connection> | null;
@@ -29,6 +19,12 @@ if (!cached) {
 }
 
 async function connectToDatabase() {
+  if (!MONGODB_URI) {
+    throw new Error(
+      'Define MONGODB_URI in .env.local or Vercel Settings'
+    );
+  }
+
   if (cached.conn) {
     return cached.conn;
   }
